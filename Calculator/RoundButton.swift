20edc6b1b72a1c5c39396f14a26d0e44a3770f10
xpexpe <xpexpe3@icloud.com>
2022-02-19 19:53:41 +0900

    2022-02-19

---
+++ Calculator/RoundButton.swift
@@ -0,0 +1,19 @@
+//
+//  RoundButton.swift
+//  Calculator
+//
+//  Created by 준현의 Macbook Pro on 2022/02/19.
+//
+
+import UIKit
+
+
+class RoundButton: UIButton {
+    @IBInspectable var isRound: Bool = false {
+        didSet {
+            if isRound {
+                self.layer.cornerRadius = self.frame.height / 2
+            }
+        }
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
+enum Operation {
+    case Add
+    case Subtract
+    case Divide
+    case Multiply
+    case nuknown
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberOutputLabel: UILabel!
+    
+    var displayNumber = ""
+    var firstOperand = ""
+    var secondOperand = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapNumberButton(_ sender: UIButton) {
+        
+    }
+    @IBAction func tapClearButton(_ sender: UIButton) {
+        
+    }
+    @IBAction func tapDotButton(_ sender: UIButton) {
+        
+    }
+    @IBAction func tapDivideButton(_ sender: UIButton) {
+        
+    }
+    @IBAction func tapMultiplyButton(_ sender: UIButton) {
+        
+    }
+    @IBAction func tapSubtractButton(_ sender: UIButton) {
+        
+    }
+    @IBAction func tapAddButton(_ sender: UIButton) {
+        
+    }
+    @IBAction func tapEqualButton(_ sender: UIButton) {
+        
+    }
 }
 
